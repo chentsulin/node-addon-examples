14d44865d7e2ad56488044e4ae511c17914174ee
Leo Chen-Tsu_Lin <lin19890808@gmail.com>
2014-11-19 12:31:47 +0800

    remove build/Release

--- hello-world/build/Release/.deps/Release/hello.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/hello.node := ./gyp-mac-tool flock ./Release/linker.lock c++ -bundle -Wl,-search_paths_first -mmacosx-version-min=10.5 -arch x86_64 -L./Release  -o Release/hello.node Release/obj.target/hello/src/hello.o -undefined dynamic_lookup
